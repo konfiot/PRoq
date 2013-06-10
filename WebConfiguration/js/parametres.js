6b30c9f33207f8e6e469fda015d655e91f0b88b8
Rémi <remim.dupre@gmail.com>
2013-06-10 20:09:19 +0200

    Design du premier formulaire : les mails
    Ca a franchement la classe, je trouve ...

--- WebConfiguration/js/parametres.js
+++ WebConfiguration/js/parametres.js
@@ -7,8 +7,10 @@ $(".leftnav").mousedown(function(e){//Quand on clic sur un élément du menu
     $(".leftnav").removeClass("active");                                //On desactive tout
     $(e.delegateTarget).addClass("active");                             //On active le bon
     
+    //---------------------
+    
     var onglet = $(e.delegateTarget).children("a").attr("href");        //On choppe l'id ciblé par le lien
-    onglet = onglet.substring(1, onglet.lenght);                        //On vire le '#'
+    onglet = onglet.substring(1, onglet.length);                        //On vire le '#'
 
     $(".hero-unit").load("forms/" + onglet + ".html", function(){ });   //On envoit la requette
 });
