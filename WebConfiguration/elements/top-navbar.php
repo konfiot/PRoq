8a0602b8ed17a695c727453006d76880072cc191
Rémi <remim.dupre@gmail.com>
2013-06-15 16:01:42 -0400

    Ajout du bouton de menu Maintenant les liens ne disparaissent plus de la barre de navigation quand la fenetre retrecis

--- WebConfiguration/elements/top-navbar.php
+++ WebConfiguration/elements/top-navbar.php
@@ -7,6 +7,11 @@
     <div class="navbar navbar-inverse navbar-fixed-top">
         <div class="navbar-inner">
             <div class="container-fluid">
+                <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
                 <a class="brand" href="index.php">Smart wake</a>
                 <div class="nav-collapse collapse">
                     <p class="navbar-text pull-right">
