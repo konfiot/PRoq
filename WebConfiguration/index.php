409e3bed6b599667ae0c1dafc2d3d1098adfdfba
Rémi <remim.dupre@gmail.com>
2013-06-27 10:19:17 -0400

    Mon 42ème commit ! En plus d'être le dernier jours qui m'a demandé de me soucier du Français, je soumet aujourd'hui mon 42ème commit. Cette responsabilité ne fut pas tâche aisé, surtout dans ce contexte historique qui est des épreuves du bac, oppressé par mon retard sur mon rythme de codativité, et par mon manque d'imagination, je me suis finalement dit qu'il vallait mieu que je fasse quelque-chose d'imparfait / d'expérimental pour demander l'avis de toute l'équipe (Arthur quoi) C'est ainsi que j'ai intégré dans le projet un caroussel assez moche mais qui, si l'idée est approuvée, être améilloré et embelli Bonne fête du 42 à tous

--- WebConfiguration/index.php
+++ WebConfiguration/index.php
@@ -10,6 +10,7 @@
     <link href="css/font-awesome.min.css" rel="stylesheet">
     
     <link href="css/main.css" rel="stylesheet">
+    <link href="css/presentation.css" rel="stylesheet">
  
   </head>
   <body>
@@ -19,6 +20,7 @@
     <!-- ----------------------------------------------------------------------------------------------- -->
     <!--                                              Corps                                              -->
     <!-- ----------------------------------------------------------------------------------------------- -->
+    
     
     <div class="container-fluid">
         <div class="row-fluid">
@@ -40,13 +42,51 @@
                     <a class="btn btn-primary btn-large pull-right" href="https://github.com/konfiot/smart-wake"><i class="icon-github icon"></i> Projet github</a>
                 </p>
             </div>
+            
+            
+            <div class="span8 offset2">
+                <div class="carousel slide" id="presentation">
+                    <div class="carousel-inner">
+                        <a class="item active" href="http://twitter.github.io/bootstrap/"> <img alt="" src="img/presentation/bootstrap.jpg"/>
+                            <div class="carousel-caption">
+                                <p>Bootstrap</p>
+                                Pour un design élégant
+                            </div>
+                        </a>
+                        <a class="item" href="http://jquery.com"> <img alt="" src="img/presentation/jquery.png"/>
+                            <div class="carousel-caption">
+                                <p>JQuery</p>
+                                Pour une gestion du javascript permetant une prise en main meilleure de l'interface
+                            </div>
+                        </a>
+                        <a class="item" href="http://www.raspberrypi.org/"> <img alt="" src="img/presentation/pi.jpg"/>
+                            <div class="carousel-caption">
+                                <p>Hébergé par un raspberry pi</p>
+                                Parce-que c'est pas cher
+                            </div>
+                        </a>
+                        <a class="item" href="http://github.com/"> <img alt="" src="img/presentation/github.jpg"/>
+                            <div class="carousel-caption">
+                                <p>Github</p>
+                                Nous a permis un travail de groupe organisé et efficace
+                            </div>
+                        </a>
+                    </div>
+                    <a class="carousel-control left" data-slide="prev" href="#presentation">‹</a> 
+                    <a class="carousel-control right" data-slide="next" href="#presentation">›</a>
+                </div>
+            </div>
         </div>
     </div>
     
     <!-- ----------------------------------------------------------------------------------------------- -->
     <!-- ----------------------------------------------------------------------------------------------- -->
-    
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    <script>
+        $(function (){
+            $('.carousel').carousel();
+        });
+    </script>
   </body>
 </html>
