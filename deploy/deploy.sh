11554b6ddc584d9fe20e56b7d302361421c70de7
konfiot <arthurtoussaint@free.fr>
2013-06-28 13:40:22 +0200

    Oui, je sais, j'y vais à tatons et ça fait plein de commits

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,6 +2,8 @@
 wget -O system.img.zip http://archlinuxarm.org/os/rpi/archlinux-hf-2013-06-15.img.zip
 unzip system.img.zip
 mkdir system
+sudo /sbin/depmod -a
+sudo modprobe loop
 sudo mount archlinux-hf-2013-06-15.img system
 ls system
 
