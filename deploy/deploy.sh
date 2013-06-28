ff0b61e47690af6d847b6177dca85f91cafaba30
konfiot <arthurtoussaint@free.fr>
2013-06-28 13:09:17 +0200

    Dézippage de l'image

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-wget http://archlinuxarm.org/os/rpi/archlinux-hf-2013-06-15.img.zip system.img.zip
+wget -O system.img.zip http://archlinuxarm.org/os/rpi/archlinux-hf-2013-06-15.img.zip
+unzip system.img.zip
+
