9b3d7d72c3ab35d0fc6db724ca57cf2784cb1d75
konfiot <arthurtoussaint@free.fr>
2013-06-28 13:33:25 +0200

    Décidément c'est dur de monter un .img ...

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,7 +2,6 @@
 wget -O system.img.zip http://archlinuxarm.org/os/rpi/archlinux-hf-2013-06-15.img.zip
 unzip system.img.zip
 mkdir system
-modprobe loop
-sudo mount -o loop archlinux-hf-2013-06-15.img system
+sudo mount archlinux-hf-2013-06-15.img system
 ls system
 
