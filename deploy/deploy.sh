8e9b535da8acdc5f2890d0fb75051b153b474912
konfiot <arthurtoussaint@free.fr>
2013-06-28 13:22:07 +0200

    Montage de l'image système

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 wget -O system.img.zip http://archlinuxarm.org/os/rpi/archlinux-hf-2013-06-15.img.zip
 unzip system.img.zip
+mkdir system
+mount -o loop archlinux-hf-2013-06-15.img system
+ls system
 
