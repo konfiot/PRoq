939bf4ab6026940f884d8f86ae19795c4ddef360
konfiot <arthurtoussaint@free.fr>
2013-06-28 13:29:09 +0200

    Ah, pitit fail, apparament faut charger un module du noyau

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,6 +2,7 @@
 wget -O system.img.zip http://archlinuxarm.org/os/rpi/archlinux-hf-2013-06-15.img.zip
 unzip system.img.zip
 mkdir system
+modprobe loop
 sudo mount -o loop archlinux-hf-2013-06-15.img system
 ls system
 
