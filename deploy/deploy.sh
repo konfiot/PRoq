1e6b0ee3453391f751071400e0bd0c271edec1e7
konfiot <arthurtoussaint@free.fr>
2013-06-29 09:33:45 +0200

    Des petits tests pour le build

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,5 +3,7 @@ wget -O system.img.zip http://archlinuxarm.org/os/rpi/archlinux-hf-2013-06-15.im
 unzip system.img.zip
 mkdir system
 sudo mount -o loop,offset=96468992 archlinux-hf-2013-06-15.img system/
+echo "list"
 ls system
+echo "endlist"
 
