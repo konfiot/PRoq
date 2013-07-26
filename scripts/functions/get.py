47ec7f3755302872edf453760b07917a211b0d41
konfiot <arthurtoussaint@free.fr>
2013-07-26 18:33:30 +0200

    Ajout du support de bazqux comme lecteur de news

--- scripts/functions/get.py
+++ scripts/functions/get.py
@@ -80,4 +80,10 @@ def news(conf) :
 			unread += i["unread"]
 		return unread
 		
-
+	elif conf["news"]["provider"] == "bazqux" :
+		req = urllib.urlopen("https://bazqux.com/accounts/ClientLogin", urllib.urlencode({"Email" : conf["news"]["user"], "Passwd": conf["news"]["passwd"]}))
+		auth_token = re.findall(r"Auth=(.+)", req.read())
+		req = urllib2.Request("https://bazqux.com/reader/api/0/unread-count?output=json", None, {"Authorization" : "GoogleLogin auth=" + auth_token[0]})
+		resp = urllib2.urlopen(req)
+		data = json.load(resp)
+		return data["unreadcounts"][0]["count"]
