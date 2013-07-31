7cfb4c9469faf6f449111b5a6689297da21cd6fc
konfiot <arthurtoussaint@free.fr>
2013-07-31 11:46:00 +0200

    On avance sur le script de déploiement

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -26,13 +26,13 @@ fi
 echo "Installing"
 qemu-system-arm -kernel kernel-qemu -cpu arm1176 -m 256 -M versatilepb -no-reboot -append "root=/dev/sda5 rw panic=1" -hda system.img -nographic -redir tcp:5555::22 &
 sleep 45
-
+ssh root:root@localhost -p 5555 "pacman -Syu ; reboot"
 
 echo "Compressing"
 gzip system.img
 
 echo "Uploading"
-rsync -e ssh system.img.gz konfiot@frs.sourceforge.net:/home/frs/project/smart-wake/Nightly/ --progress
+yes yes | rsync -e ssh system.img.gz konfiot@frs.sourceforge.net:/home/frs/project/smart-wake/Nightly/ --progress
 
 echo "Cleaning"
 rm system.img
