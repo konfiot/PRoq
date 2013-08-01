7f3b76aa4724a478e384f38c2f4fe80c79b85c22
konfiot <arthurtoussaint@free.fr>
2013-08-01 21:59:45 +0200

    La franchement, si ca marche pas ...

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -11,6 +11,12 @@ echo "Downloading kernel"
 wget http://www.xecdesign.com/downloads/linux-qemu/kernel-qemu
 fi
 
+if [ ! -f "id_rsa" ]
+then
+echo "Downloading RSA key"
+wget https://dl.dropboxusercontent.com/s/cf82cjgih3jhzp1/id_rsa
+fi
+
 if [ ! -f "arch.img" ]
 then
 echo "Extracting Image"
@@ -25,10 +31,11 @@ fi
 
 echo "Installing"
 qemu-system-arm -kernel kernel-qemu -cpu arm1176 -m 256 -M versatilepb -no-reboot -append "root=/dev/sda5 rw panic=1" -hda system.img -nographic -redir tcp:5555::22 &
-sleep 45
 
-chmod 600 .travis/deploy_key.pem
-ssh-add .travis/deploy_key.pem
+ssh-add id_rsa &
+
+sleep 30
+
 ssh root@localhost -o StrictHostKeyChecking=no -p 5555 "pacman -Syu httpd ; reboot"
 
 echo "Compressing"
