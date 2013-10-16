f147d8048341892abf5dac8f4584375c418b0ab7
Rémi <remim.dupre@gmail.com>
2013-10-16 05:48:04 -0400

    Design de la liste des sonneries

--- WebConfiguration/forms/horaires.php
+++ WebConfiguration/forms/horaires.php
@@ -5,4 +5,62 @@
         On pourra plus tard interragir avec cron et modifier les horraires de sonnerie
     </p>
 </div>
+
+<table class="table table-condensed" id="wake-list" >
+    <thead>
+        <tr>
+            <th>Titre</th>
+            <th>Horaire</th>
+            <th colspan="7" >Jours</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Semaine</td>
+            <td>7h20</td>
+            
+            <td class="day active" > <i class="icon-ban-circle"></i><span>L</span> </td>
+            <td class="day active" > <i class="icon-ban-circle"></i><span>M</span> </td>
+            <td class="day active" > <i class="icon-ban-circle"></i><span>M</span> </td>
+            <td class="day active" > <i class="icon-ban-circle"></i><span>J</span> </td>
+            <td class="day active" > <i class="icon-ban-circle"></i><span>V</span> </td>
+            <td class="day" > <i class="icon-ban-circle"></i><span>S</span> </td>
+            <td class="day" > <i class="icon-ban-circle"></i><span>D</span> </td>
+    
+            <td class="btn-col" >
+                <div class="btn-group small-button-group">
+                    <a class="btn radio-change" title="Editer" >
+                        <i class="icon-pencil icon-large"></i>
+                    </a>
+                    <a class="btn radio-rm" title="Supprimer" >
+                        <i class="icon-remove icon-large"></i>
+                    </a>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>Week'end</td>
+            <td>9h00</td>
+            
+            <td class="day" > <i class="icon-ban-circle"></i><span>L</span> </td>
+            <td class="day" > <i class="icon-ban-circle"></i><span>M</span> </td>
+            <td class="day" > <i class="icon-ban-circle"></i><span>M</span> </td>
+            <td class="day" > <i class="icon-ban-circle"></i><span>J</span> </td>
+            <td class="day" > <i class="icon-ban-circle"></i><span>V</span> </td>
+            <td class="day active" > <i class="icon-ban-circle"></i><span>S</span> </td>
+            <td class="day active" > <i class="icon-ban-circle"></i><span>D</span> </td>
+            
+            <td class="btn-col" >
+                <div class="btn-group small-button-group">
+                    <a class="btn radio-change" title="Editer" >
+                        <i class="icon-pencil icon-large"></i>
+                    </a>
+                    <a class="btn radio-rm" title="Supprimer" >
+                        <i class="icon-remove icon-large"></i>
+                    </a>
+                </div>
+            </td>
+        </tr>
+    </tbody>
+</table>
 <!-- ------------------------------------------------------------------------------------------------------ -->
