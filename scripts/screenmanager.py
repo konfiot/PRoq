80ffdfe0a52894b48711834e4315e0593cf31de7
konfiot <arthurtoussaint@free.fr>
2013-12-11 19:31:50 +0100

    Quand y'a pas de data, on affiche rien

--- scripts/screenmanager.py
+++ scripts/screenmanager.py
@@ -46,69 +46,77 @@ def get_data():
 	s.connect("mastersocket")
 	s.send(json.dumps({"request": "get_data"}))
 	data = s.recv(8192)
-	(data, data_forecast, unread, news, cal) = json.loads(data)
-
-	return (data, data_forecast, unread, news, cal)
+	raw = json.loads(data)
+	if raw :
+		(data, data_forecast, unread, news, cal) = raw
+		return (data, data_forecast, unread, news, cal)
+	else :
+		return False
 
 def update(): 
-	(data, data_forecast, unread, news, cal) = get_data()
-	image_weather = pygame.image.load("images/weather/" + data_forecast["list"][0]["weather"][0]["icon"] + ".png")
-	# Collage des images
-	background.fill((0, 0, 0))
+	raw = get_data()
+	if raw :
+		(data, data_forecast, unread, news, cal) = raw
 
-	background.blit(image_weather, image_weather.get_rect())
-	background.blit(image_temp, image_temp.get_rect(centerx=130))
-	background.blit(image_rise, image_rise.get_rect(centerx=190, centery=22))
-	background.blit(image_set, image_set.get_rect(centerx=260, centery=22))
-	background.blit(image_mail, image_mail.get_rect(centerx=130, centery=60))
-	background.blit(image_news, image_news.get_rect(centerx=190, centery=60))
-	background.blit(image_cal, image_cal.get_rect(centerx=260, centery=60))
+		image_weather = pygame.image.load("images/weather/" + data_forecast["list"][0]["weather"][0]["icon"] + ".png")
+		# Collage des images
+		background.fill((0, 0, 0))
 
-	# On ajoute l'heure
+		background.blit(image_weather, image_weather.get_rect())
+		background.blit(image_temp, image_temp.get_rect(centerx=130))
+		background.blit(image_rise, image_rise.get_rect(centerx=190, centery=22))
+		background.blit(image_set, image_set.get_rect(centerx=260, centery=22))
+		background.blit(image_mail, image_mail.get_rect(centerx=130, centery=60))
+		background.blit(image_news, image_news.get_rect(centerx=190, centery=60))
+		background.blit(image_cal, image_cal.get_rect(centerx=260, centery=60))
 
-	text_time = font_time.render(datetime.today().strftime("%H:%M"), 1, (255, 255, 255))
-	text_time_pos = text_time.get_rect(centerx=157, y=112)
-	background.blit(text_time, text_time_pos)
+		# On ajoute l'heure
 
-	# On ajoute la température
+		text_time = font_time.render(datetime.today().strftime("%H:%M"), 1, (255, 255, 255))
+		text_time_pos = text_time.get_rect(centerx=157, y=112)
+		background.blit(text_time, text_time_pos)
 
-	text_temp = font.render(str(round(data_forecast["list"][0]["temp"]["day"], 1)), 1, (255, 255, 255))
-	text_temp_pos = text_temp.get_rect(x=145, y=18)
-	background.blit(text_temp, text_temp_pos)
+		# On ajoute la température
 
-	# On ajoute les image de lever et de coucher du soleil
+		text_temp = font.render(str(round(data_forecast["list"][0]["temp"]["day"], 1)), 1, (255, 255, 255))
+		text_temp_pos = text_temp.get_rect(x=145, y=18)
+		background.blit(text_temp, text_temp_pos)
 
-	text_time_rise = font.render(datetime.fromtimestamp(data["sys"]["sunrise"]).strftime("%H:%M"), 1, (255, 255, 255))
-	text_time_rise_pos = text_time_rise.get_rect(x=210, y=18)
-	background.blit(text_time_rise, text_time_rise_pos)
+		# On ajoute les image de lever et de coucher du soleil
 
-	text_time_set = font.render(datetime.fromtimestamp(data["sys"]["sunset"]).strftime("%H:%M"), 1, (255, 255, 255))
-	text_time_set_pos = text_time_set.get_rect(x=280, y=18)
-	background.blit(text_time_set, text_time_set_pos)
+		text_time_rise = font.render(datetime.fromtimestamp(data["sys"]["sunrise"]).strftime("%H:%M"), 1, (255, 255, 255))
+		text_time_rise_pos = text_time_rise.get_rect(x=210, y=18)
+		background.blit(text_time_rise, text_time_rise_pos)
 
-	# On ajoute le compteur des mails non lus
+		text_time_set = font.render(datetime.fromtimestamp(data["sys"]["sunset"]).strftime("%H:%M"), 1, (255, 255, 255))
+		text_time_set_pos = text_time_set.get_rect(x=280, y=18)
+		background.blit(text_time_set, text_time_set_pos)
 
-
-	text_mail = font.render(str(unread), 1, (255, 255, 255))
-	text_mail_pos = text_mail.get_rect(x=145, y=55)
-	background.blit(text_mail, text_mail_pos)
-
-	# On met aussi les news
+		# On ajoute le compteur des mails non lus
 
 
-	text_news = font.render(str(news), 1, (255, 255, 255))
-	text_news_pos = text_news.get_rect(x=210, y=55)
-	background.blit(text_news, text_news_pos)
+		text_mail = font.render(str(unread), 1, (255, 255, 255))
+		text_mail_pos = text_mail.get_rect(x=145, y=55)
+		background.blit(text_mail, text_mail_pos)
 
-	# On ajoute le compteur d'évènements dans la journée
+		# On met aussi les news
 
 
-	text_cal = font.render(str(cal[0]), 1, (255, 255, 255))
-	text_cal_pos = text_cal.get_rect(x=280, y=55)
-	background.blit(text_cal, text_cal_pos)
+		text_news = font.render(str(news), 1, (255, 255, 255))
+		text_news_pos = text_news.get_rect(x=210, y=55)
+		background.blit(text_news, text_news_pos)
 
-	screen.blit(background, (0, 0))
-	pygame.display.flip()
+		# On ajoute le compteur d'évènements dans la journée
+
+
+		text_cal = font.render(str(cal[0]), 1, (255, 255, 255))
+		text_cal_pos = text_cal.get_rect(x=280, y=55)
+		background.blit(text_cal, text_cal_pos)
+
+		screen.blit(background, (0, 0))
+		pygame.display.flip()
+	else :
+		return False
 
 # Boucle de rafraichissement
 
