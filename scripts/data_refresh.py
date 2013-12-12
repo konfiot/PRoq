0736785d89bcfc8900a68210380ca474ee4ed59f
konfiot <arthurtoussaint@free.fr>
2013-12-12 18:47:57 +0100

    On rajoute un try except pour gérer les erreurs dans data_refresh.py et stabiliser le tout, fix #54

--- scripts/data_refresh.py
+++ scripts/data_refresh.py
@@ -10,11 +10,13 @@ conf_file = open("../conf/wake.json")
 conf = json.load(conf_file)
 
 while 1 :
-	(data, data_forecast) = get.weather(conf)
-	unread = get.mail(conf)
-	news = get.news(conf)
-	cal = get.calendar(conf)
-	
+	try :
+		(data, data_forecast) = get.weather(conf)
+		unread = get.mail(conf)
+		news = get.news(conf)
+		cal = get.calendar(conf)
+	except :
+		continue;
 	s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 	s.connect("mastersocket")
 	s.send(json.dumps({"request": "new_data", "content": [data, data_forecast, unread, news, cal]}))
