1508811642044e8bc246608b12b426920d768cbd
Arthur <konfiot@users.noreply.github.com>
2013-12-18 20:36:14 +0100

    On y était presque !

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -46,7 +46,7 @@ sudo make install
 cd 
 wget https://aur.archlinux.org/packages/sv/svox-pico-git/svox-pico-git.tar.gz ;
 tar -xvzf svox-pico-git.tar.gz ;
-sed 's/configure/configure CC=gcc-arm-linux-gnueabihf/' -i svox-pico-git/PKGBUILD ;
+sed 's/configure /configure CC=gcc-arm-linux-gnueabihf/' -i svox-pico-git/PKGBUILD ;
 cd svox-pico-git ;
 makepkg;
 
