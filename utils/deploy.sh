5245af1db5c68d38525efb114d11e5ec8753f63a
Arthur <konfiot@users.noreply.github.com>
2013-12-22 10:58:13 +0100

    On tatonne

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -46,7 +46,7 @@ sudo make install
 cd 
 wget https://aur.archlinux.org/packages/sv/svox-pico-git/svox-pico-git.tar.gz ;
 tar -xvzf svox-pico-git.tar.gz ;
-sed 's/configure /configure --host=arm-linux-gnueabihf /' -i svox-pico-git/PKGBUILD ;
+sed 's/configure /configure --host=arm-linux-gnueabihf --enable-static --disable-shared /' -i svox-pico-git/PKGBUILD ;
 cd svox-pico-git ;
 makepkg;
 
