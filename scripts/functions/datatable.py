f2ee7bc578d4f75f2fc5ef98f968b53507bd879c
konfiot <arthurtoussaint@free.fr>
2013-12-22 22:08:52 +0100

    Gestion modulaire du tableau de données, fix #59

---
+++ scripts/functions/datatable.py
@@ -0,0 +1,29 @@
+import pygame
+import time
+import render
+
+class DataTable (object) :
+	def __init__ (self, background, font, top, left, width, height, rows, cols, gap_y, gap_x) :
+		self.background = background
+		self.top = int(top)
+		self.left = int(left)
+		self.width = int(width)
+		self.height = int(height)
+		self.item_width = int((width -  ((cols-1) * gap_x)) / cols)
+		self.item_height = int((height - (rows * gap_y)) / rows)
+		self.rows = int(rows)
+		self.cols = int(cols)
+		self.font = font
+		self.gap_y = gap_y
+		self.gap_x = gap_x
+
+	def update(self, data) :
+		surface = pygame.Surface((self.width, self.height))
+		surface.fill((128, 128, 128))
+		for i in range(len(data)) :
+			col = i % self.cols
+			row = int(i / self.cols)
+			surface.blit(data[i]["image"], data[i]["image"].get_rect(top = (row * self.item_height) + self.gap_y, left = (col * self.item_width)))
+			render.render(data[i]["data"], self.font, surface, (255,255,255), (col * self.item_width) + data[i]["image"].get_width() + self.gap_x, (row * self.item_height) + self.gap_y, self.item_width - data[i]["image"].get_width() - (2*self.gap_x), self.item_height - self.gap_y) 
+
+		self.background.blit(surface, pygame.Rect(self.left, self.top, self.width, self.height))
--- scripts/screenmanager.py
+++ scripts/screenmanager.py
@@ -7,7 +7,7 @@ import urllib
 import StringIO
 import time
 from datetime import datetime, date
-from functions import get, menu, render
+from functions import get, menu, render, datatable
 import sys
 import socket
 
@@ -25,7 +25,7 @@ conf = json.load(conf_file)
 
 # Définition des polices
 font_filename = pygame.font.match_font(conf["general"]["font"])
-font = pygame.font.Font(font_filename, 14)
+font = pygame.font.Font(font_filename, 135)
 font_time = pygame.font.Font(font_filename, 135)
 
 
@@ -40,6 +40,7 @@ image_cal = pygame.image.load("images/misc/cal.png")
 
 
 config_menu = menu.Menu(screen, ["Modifier l'heure", "Durée du snooze", "Modifier l'action du snooze"], font)
+table = datatable.DataTable(background, font, 0, 160, 160, 120, 3, 2, 10, 5)
 
 def get_data(): 
 	s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
@@ -62,7 +63,9 @@ def update():
 		# Collage des images
 		background.fill((0, 0, 0))
 
-		background.blit(image_weather, image_weather.get_rect())
+		table.update([{"image": image_temp, "data": str(round(data_forecast["list"][0]["temp"]["day"], 1))}, {"image": image_rise, "data": datetime.fromtimestamp(data["sys"]["sunrise"]).strftime("%H:%M")}, {"image": image_rise, "data": datetime.fromtimestamp(data["sys"]["sunset"]).strftime("%H:%M")}, {"image": image_mail, "data": str(unread)}, {"image": image_news, "data": str(news)}, {"image": image_cal, "data": str(cal[0])}])
+
+		"""background.blit(image_weather, image_weather.get_rect())
 		background.blit(image_temp, image_temp.get_rect(centerx=130))
 		background.blit(image_rise, image_rise.get_rect(centerx=190, centery=22))
 		background.blit(image_set, image_set.get_rect(centerx=260, centery=22))
@@ -71,60 +74,30 @@ def update():
 		background.blit(image_cal, image_cal.get_rect(centerx=260, centery=60))
 
 		# On ajoute l'heure
-
-		"""text_time = font_time.render(datetime.today().strftime("%H:%M"), 1, (255, 255, 255))
-		text_height = font_time.get_ascent()
-		text_time = text_time.subsurface(pygame.Rect(0, (text_time.get_height() - text_height) / 2 , text_time.get_width(), text_height))
-		background.blit(pygame.transform.smoothscale(text_time, (320, 120)), pygame.Rect(0, 120, 320, 120))"""
 		
 		render.render(datetime.today().strftime("%H:%M"), font_time, background, (255,255,255), 0, 120, 320, 120)
+		
 		# On ajoute la température
 
-		"""text_temp = font.render(str(round(data_forecast["list"][0]["temp"]["day"], 1)), 1, (255, 255, 255))
-		text_temp_pos = text_temp.get_rect(x=145, y=18)
-		background.blit(text_temp, text_temp_pos)"""
-
 		render.render(str(round(data_forecast["list"][0]["temp"]["day"], 1)), font, background, (255,255,255), 140, 20, 30, 14)
+		
 		# On ajoute les image de lever et de coucher du soleil
 
-		"""text_time_rise = font.render(datetime.fromtimestamp(data["sys"]["sunrise"]).strftime("%H:%M"), 1, (255, 255, 255))
-		text_time_rise_pos = text_time_rise.get_rect(x=210, y=18)
-		background.blit(text_time_rise, text_time_rise_pos)"""
-
 		render.render(datetime.fromtimestamp(data["sys"]["sunrise"]).strftime("%H:%M"), font, background, (255,255,255), 210, 20, 30, 14)
-
-		"""text_time_set = font.render(datetime.fromtimestamp(data["sys"]["sunset"]).strftime("%H:%M"), 1, (255, 255, 255))
-		text_time_set_pos = text_time_set.get_rect(x=280, y=18)
-		background.blit(text_time_set, text_time_set_pos)"""
 
 		render.render(datetime.fromtimestamp(data["sys"]["sunrise"]).strftime("%H:%M"), font, background, (255,255,255), 280, 20, 30, 14)
 
 		# On ajoute le compteur des mails non lus
 
-
-		"""text_mail = font.render(str(unread), 1, (255, 255, 255))
-		text_mail_pos = text_mail.get_rect(x=145, y=55)
-		background.blit(text_mail, text_mail_pos)"""
-
 		render.render(str(unread), font, background, (255,255,255), 140, 52, 30, 14)
 
 		# On met aussi les news
-
-
-		"""text_news = font.render(str(news), 1, (255, 255, 255))
-		text_news_pos = text_news.get_rect(x=210, y=55)
-		background.blit(text_news, text_news_pos)"""
 
 		render.render(str(news), font, background, (255,255,255), 210, 52, 30, 14)
 
 		# On ajoute le compteur d'évènements dans la journée
 
-
-		"""text_cal = font.render(str(cal[0]), 1, (255, 255, 255))
-		text_cal_pos = text_cal.get_rect(x=280, y=55)
-		background.blit(text_cal, text_cal_pos)"""
-
-		render.render(str(cal[0]), font, background, (255,255,255), 280, 52, 30, 14)
+		render.render(str(cal[0]), font, background, (255,255,255), 280, 52, 30, 14)"""
 
 		screen.blit(background, (0, 0))
 		pygame.display.flip()
