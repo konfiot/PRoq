0693b0c70fb75b8bab84c74414f185d57cf3d434
Arthur <konfiot@users.noreply.github.com>
2014-01-30 11:25:28 +0100

    Gestion du codeur rotatif dans le sensors.py, contribue a #56 et #57

--- scripts/sensors.py
+++ scripts/sensors.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import RPi.GPIO as GPIO
+import gaugette.rotary_encoder
+import gaugette.switch 
 
 def readadc(adcnum, clockpin, mosipin, misopin, cspin):
 	if ((adcnum > 7) or (adcnum < 0)):
@@ -42,18 +44,29 @@ SPIMISO = 23
 SPIMOSI = 24
 SPICS = 25
 
+A_PIN = 7
+B_PIN = 9
+SW_PIN = 8 
+
 GPIO.setup(SPIMOSI, GPIO.OUT)
 GPIO.setup(SPIMISO, GPIO.IN)
 GPIO.setup(SPICLK, GPIO.OUT)
 GPIO.setup(SPICS, GPIO.OUT)
+
+encoder = gaugette.rotary_encoder.RotaryEncoder.Worker(A_PIN, B_PIN)
+encoder.start()
+switch = gaugette.switch.Switch(SW_PIN)
+last_state = None 
 
 prox_state = False
 
 while True : 
 	adcval = readadc(0, SPICLK, SPIMOSI, SPIMISO, SPICS)
 	millivolts = adcval * ( 3300.0 / 1024.0)
-
-	if milivolts >= THRESOLD and prox_state == False :
+	delta = encoder.get_delta()
+	sw_state = switch.get_state()
+	
+	if milivolts >= THRESOLD and prox_state == False : # TODO : A simplifier
 		s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 		s.connect("mastersocket")
 		s.send(json.dumps({"request": "set_prox_state", "content": True}))
@@ -64,5 +77,15 @@ while True :
 		s.send(json.dumps({"request": "set_prox_state", "content": False}))
 		prox_state = False
 
-
+	if delta!=0:
+		s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+		s.connect("mastersocket")
+		s.send(json.dumps({"request": "set_delta", "content": str(delta)}))
+	
+	if sw_state != last_state:
+		s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+		s.connect("mastersocket")
+		s.send(json.dumps({"request": "set_switch_state", "content": str(sw_state)}))
+		last_state = sw_state
+	
 	time.sleep(0.05)
