ffa06a41aab56945af7cd3008d1b55a88abaa0e0
Arthur <konfiot@users.noreply.github.com>
2014-02-03 16:16:42 +0100

    fix dépendances

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -47,18 +47,13 @@ scp -r -i id_rsa -o StrictHostKeyChecking=no -P 5555 ./WebConfiguration/ root@lo
 echo "Installing"
 ssh root@localhost -o StrictHostKeyChecking=no -p 5555 -i id_rsa "export EDITOR=cat ; 
 yes | pacman -R heirloom-mailx ;
-yes | pacman -Syu popt python2-numpy lighttpd python mpd python2-pygame php-cgi php python2-pip git alsa-lib alsa-firmware ttf-dejavu ttf-droid ttf-ubuntu-font-family ttf-linux-libertine ttf-liberation ttf-junicode ttf-freefont ttf-inconsolata ttf-indic-otf ttf-cheapskate ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming ;
+yes | pacman -Syu popt python2-numpy lighttpd python mpd python2-pygame php-cgi php python2-pip git alsa-lib alsa-firmware make gcc ttf-dejavu ttf-droid ttf-ubuntu-font-family ttf-linux-libertine ttf-liberation ttf-junicode ttf-freefont ttf-inconsolata ttf-indic-otf ttf-cheapskate ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming ;
 wget https://dl.dropboxusercontent.com/s/blr71qll61vclex/svox-pico-git-7cb980c-1-armv6h.pkg.tar.xz
 yes | pacman -U svox-pico-git-*.pkg.tar.xz &
-yes | pip2.7 install icalendar python-mpd2 RPi.GPIO wiringpi2 netsyslog ; 
-git clone git://git.drogon.net/wiringPi
-cd wiringPi
-./build
-cd ..
-git clone git://github.com/guyc/py-gaugette.git
+yes | pip2.7 install icalendar python-mpd2 wiringpi2 ; 
+git clone https://github.com/guyc/py-gaugette.git
 cd py-gaugette
-git checkout wiringpi2
-sudo python setup.py install # skip this step if you do not want the library installed
+python setup.py install 
 cd ..
 mkdir /etc/lighttpd/conf.d ;
 mkdir /var/lib/mpd/music ; touch /var/lib/mpd/mpd.db ;
