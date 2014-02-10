d0fede5efd299ed4adc3aaf5194326b430ace0ed
Arthur <konfiot@users.noreply.github.com>
2014-02-10 14:14:00 +0100

    Décommentage des trucs utiles

--- scripts/sensors.py
+++ scripts/sensors.py
@@ -5,8 +5,8 @@ import spidev
 import time
 import socket
 import json
-#import gaugette.rotary_encoder
-#import gaugette.switch 
+import gaugette.rotary_encoder
+import gaugette.switch 
 
 def ReadChannel(channel):
 	adc = spi.xfer2([1,(8+channel)<<4,0])
@@ -22,18 +22,18 @@ A_PIN = 7
 B_PIN = 9
 SW_PIN = 8 
 
-#encoder = gaugette.rotary_encoder.RotaryEncoder.Worker(A_PIN, B_PIN)
-#encoder.start()
-#switch = gaugette.switch.Switch(SW_PIN)
-last_state = None 
+encoder = gaugette.rotary_encoder.RotaryEncoder.Worker(A_PIN, B_PIN)
+encoder.start()
+switch = gaugette.switch.Switch(SW_PIN)
+last_state = False
 THRESOLD = 700
 prox_state = 0
 
 while True : 
 	adcval = ReadChannel(0)
 	milivolts = adcval * ( 3300.0 / 1024.0)
-	#delta = encoder.get_delta()
-	#sw_state = switch.get_state()
+	delta = encoder.get_delta()
+	sw_state = switch.get_state()
 	
 	if milivolts >= THRESOLD and prox_state == 0 : # TODO : A simplifier
 		s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
@@ -46,15 +46,15 @@ while True :
 		s.send(json.dumps({"request": "set_prox_state", "content": 0}))
 		prox_state = 0
 
-#	if delta!=0:
-#		s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-#		s.connect("mastersocket")
-#		s.send(json.dumps({"request": "set_delta", "content": str(delta)}))
-#	
-#	if sw_state != last_state:
-#		s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-#		s.connect("mastersocket")
-#		s.send(json.dumps({"request": "set_switch_state", "content": str(sw_state)}))
-#		last_state = sw_state
+	if delta!=0:
+		s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+		s.connect("mastersocket")
+		s.send(json.dumps({"request": "set_delta", "content": str(delta)}))
+	
+	if sw_state != last_state:
+		s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+		s.connect("mastersocket")
+		s.send(json.dumps({"request": "set_switch_state", "content": str(sw_state)}))
+		last_state = sw_state
 	
 	time.sleep(0.05)
