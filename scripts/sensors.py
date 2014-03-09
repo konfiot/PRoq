380572b464d06b87cfc0dec0fed8e5dcd5112015
konfiot <arthurtoussaint@free.fr>
2014-03-09 20:33:06 +0100

    Gestion manuelle du switch et de l'encodeur

--- scripts/sensors.py
+++ scripts/sensors.py
@@ -5,8 +5,7 @@ import spidev
 import time
 import socket
 import json
-import gaugette.rotary_encoder
-import gaugette.switch 
+import wiringpi2
 
 def ReadChannel(channel):
 	adc = spi.xfer2([1,(8+channel)<<4,0])
@@ -22,19 +21,37 @@ A_PIN = 7
 B_PIN = 9
 SW_PIN = 8 
 
-encoder = gaugette.rotary_encoder.RotaryEncoder.Worker(A_PIN, B_PIN)
-encoder.start()
-switch = gaugette.switch.Switch(SW_PIN)
 last_state = False
 THRESOLD = 700
 prox_state = 0
 
+r_seq = 0
+old_r_seq = 0
+delta = 0
+old_delta = 0
+
+wiringpi2.wiringPiSetupGpio()
+wiringpi2.pinMode(A_PIN,0)
+wiringpi2.pinMode(B_PIN,0)
+wiringpi2.pinMode(SW_PIN,0)
+
 while True : 
 	adcval = ReadChannel(0)
 	milivolts = adcval * ( 3300.0 / 1024.0)
-	delta = encoder.get_delta()
 	sw_state = switch.get_state()
-	
+	a_state = wiringpi2.digitalRead(A_PIN)
+	b_state = wiringpi2.digitalRead(B_PIN)
+	sw_state = wiringpi2.digitalRead(SW_PIN)
+
+        r_seq = (a_state ^ b_state) | b_state << 1
+
+	if r_seq != old_r_seq:
+		delta = (r_seq - old_r_seq) % 4
+		if delta==3:
+			delta = -1
+		elif delta==2:
+			delta = int(math.copysign(delta, old_delta))
+
 	if milivolts >= THRESOLD and prox_state == 0 : # TODO : A simplifier
 		s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 		s.connect("mastersocket")
@@ -55,6 +72,10 @@ while True :
 		s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 		s.connect("mastersocket")
 		s.send(json.dumps({"request": "set_switch_state", "content": str(sw_state)}))
-		last_state = sw_state
+	
+	last_state = sw_state
+	
+	old_r_seq = r_seq
+	old_delta = delta
 	
 	time.sleep(0.05)
