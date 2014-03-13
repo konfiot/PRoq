10b6a0f8d7bc21f1114af9c78208c03373f9de1a
konfiot <arthurtoussaint@free.fr>
2014-03-13 17:34:10 +0100

    On uploade que si la build a réussi

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -46,46 +46,43 @@ scp -r -i id_rsa -o StrictHostKeyChecking=no -P 5555 ./WebConfiguration/ root@lo
 
 echo "Installing"
 ssh root@localhost -o StrictHostKeyChecking=no -p 5555 -i id_rsa "export EDITOR=cat ; 
-yes | pacman -R heirloom-mailx ;
-yes | pacman -Syu popt python2-numpy lighttpd python mpd python2-pygame php-cgi php sudo python2-pip git alsa-lib alsa-firmware make gcc ttf-dejavu ttf-droid ttf-ubuntu-font-family ttf-linux-libertine ttf-liberation ttf-junicode ttf-freefont ttf-inconsolata ttf-indic-otf ttf-cheapskate ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming ;
+yes | pacman -R heirloom-mailx &&
+yes | pacman -Syu popt python2-numpy lighttpd python mpd python2-pygame php-cgi php sudo python2-pip git alsa-lib alsa-firmware make gcc ttf-dejavu ttf-droid ttf-ubuntu-font-family ttf-linux-libertine ttf-liberation ttf-junicode ttf-freefont ttf-inconsolata ttf-indic-otf ttf-cheapskate ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming &&
 wget https://dl.dropboxusercontent.com/s/blr71qll61vclex/svox-pico-git-7cb980c-1-armv6h.pkg.tar.xz
 yes | pacman -U svox-pico-git-*.pkg.tar.xz &
-yes | pip2.7 install icalendar python-mpd2  ; 
-git clone git://git.drogon.net/wiringPi
-cd wiringPi
-./build
-cd ..
-git clone https://github.com/Gadgetoid/WiringPi2-Python.git
-cd WiringPi2-Python/
-python2.7 setup.py install
-cd ..
-mkdir /etc/lighttpd/conf.d ;
-mkdir /var/lib/mpd/music ; touch /var/lib/mpd/mpd.db ;
-chown -R mpd:mpd  /var/lib/mpd/ ; echo 'music_directory \"/var/lib/mpd/music\"' >> /etc/mpd.conf ;
-echo 'server.modules += ( \"mod_fastcgi\" ) index-file.names += ( \"index.php\" ) fastcgi.server = ( \".php\" => ((                    \"bin-path\" => \"/usr/bin/php-cgi\",                    \"socket\" => \"/tmp/php.socket\",                    \"max-procs\" => 2,                    \"bin-environment\" => (                      \"PHP_FCGI_CHILDREN\" => \"16\",                      \"PHP_FCGI_MAX_REQUESTS\" => \"10000\"                    ),                    \"bin-copy-environment\" => (                   \"PATH\", \"SHELL\", \"USER\"                    ),                    \"broken-scriptfilename\" => \"enable\"                )))' >> /etc/lighttpd/conf.d/fastcgi.conf ;
-echo 'include \"conf.d/fastcgi.conf\"' >> /etc/lighttpd/lighttpd.conf ;
+yes | pip2.7 install icalendar python-mpd2  && 
+git clone git://git.drogon.net/wiringPi &&
+cd wiringPi &&
+./build &&
+cd .. &&
+git clone https://github.com/Gadgetoid/WiringPi2-Python.git &&
+cd WiringPi2-Python/ &&
+python2.7 setup.py install &&
+cd .. &&
+mkdir /etc/lighttpd/conf.d &&
+mkdir /var/lib/mpd/music && touch /var/lib/mpd/mpd.db  &&
+chown -R mpd:mpd  /var/lib/mpd/ && echo 'music_directory \"/var/lib/mpd/music\"' >> /etc/mpd.conf &&
+echo 'server.modules += ( \"mod_fastcgi\" ) index-file.names += ( \"index.php\" ) fastcgi.server = ( \".php\" => ((                    \"bin-path\" => \"/usr/bin/php-cgi\",                    \"socket\" => \"/tmp/php.socket\",                    \"max-procs\" => 2,                    \"bin-environment\" => (                      \"PHP_FCGI_CHILDREN\" => \"16\",                      \"PHP_FCGI_MAX_REQUESTS\" => \"10000\"                    ),                    \"bin-copy-environment\" => (                   \"PATH\", \"SHELL\", \"USER\"                    ),                    \"broken-scriptfilename\" => \"enable\"                )))' >> /etc/lighttpd/conf.d/fastcgi.conf &&
+echo 'include \"conf.d/fastcgi.conf\"' >> /etc/lighttpd/lighttpd.conf &&
 echo '[Unit]
 After=network.target
 [Service]
 Type=forking
 ExecStart=/root/start.sh
 [Install]
-WantedBy=multi-user.target' > /etc/systemd/system/proq.service ;
-systemctl enable lighttpd mpd proq ;
-ln -s /root/utils/update.sh /etc/cron.weekly/
-mv ~/WebConfiguration/* /srv/http/ ;
-mkdir /srv/http/conf/ ;
-ln -s /srv/http/conf/ /root/ ;
-rm .ssh/authorized_keys .ssh/known_hosts ;
-sed -i 's/sda/mmcblk0p/' /etc/fstab ;
-sed -i -e 's/;extension=openssl.so/extension=openssl.so/g' /etc/php/php.ini;
+WantedBy=multi-user.target' > /etc/systemd/system/proq.service &&
+systemctl enable lighttpd mpd proq &&
+ln -s /root/utils/update.sh /etc/cron.weekly/ &&
+mv ~/WebConfiguration/* /srv/http/ &&
+mkdir /srv/http/conf/ &&
+ln -s /srv/http/conf/ /root/ &&
+rm .ssh/authorized_keys .ssh/known_hosts &&
+history -c &&
+sed -i 's/sda/mmcblk0p/' /etc/fstab &&
+sed -i -e 's/;extension=openssl.so/extension=openssl.so/g' /etc/php/php. &&
 reboot"
 
 echo "Compressing"
 pxz system.img
 
 echo "Uploading"
-rsync -e "ssh -o StrictHostKeyChecking=no -i id_rsa_sourceforge" system.img.xz konfiot@frs.sourceforge.net:/home/frs/project/smart-wake/Nightly/ --progress
-
-echo "Cleaning"
-rm system.img.xz
