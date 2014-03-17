a463ff5d31eb60a4c2d3a2b87b02be584323df54
Rémi <remim.dupre@gmail.com>
2014-03-17 14:08:32 +0100

    Rajout de dépendances

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -50,7 +50,7 @@ yes | pacman -R heirloom-mailx &&
 yes | pacman -Syu popt python2-numpy lighttpd python mpd python2-pygame php-cgi php sudo python2-pip git alsa-lib alsa-firmware make gcc ttf-dejavu ttf-droid ttf-ubuntu-font-family ttf-linux-libertine ttf-liberation ttf-junicode ttf-freefont ttf-inconsolata ttf-indic-otf ttf-cheapskate ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming &&
 wget https://dl.dropboxusercontent.com/s/blr71qll61vclex/svox-pico-git-7cb980c-1-armv6h.pkg.tar.xz
 yes | pacman -U svox-pico-git-*.pkg.tar.xz &
-yes | pip2.7 install icalendar python-mpd2  && 
+yes | pip2.7 install icalendar python-mpd2 spidev pyalsaaudio  && 
 git clone git://git.drogon.net/wiringPi &&
 cd wiringPi &&
 ./build &&
